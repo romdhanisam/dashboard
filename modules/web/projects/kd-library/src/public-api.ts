2ec127987ccc2180de8f3784db30d26581d62da7
Samir Romdhani <romdhanisamir94@gmail.com>
2023-10-08 22:06:25 +0200

    Build kd-library package - Experimental test [Support angular/material v16.x.x (mdc migration)]

---
+++ modules/web/projects/kd-library/src/public-api.ts
@@ -0,0 +1,26 @@
+// Copyright 2017 The Kubernetes Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*
+ * Public API Surface of kd-library
+ */
+
+// Main service and Component
+export * from './lib/kd-library.service';
+export * from './lib/kd-library.component';
+// Main Module
+export * from './lib/kd-library.module';
+// Components
+export * from './lib/components/chips/component';
+export * from './lib/components/kd-chips-namespace-settings/component';
